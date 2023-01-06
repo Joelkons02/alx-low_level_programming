616d854c87213b14f295d644b3183bf5b62b28c4
Joelkons02 <118935303+Joelkons02@users.noreply.github.com>
2023-01-06 03:34:04 +0100

    init commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
+int wildmode(char *s1, char *s2);
+int exactmode(char *s1, char *s2);
 
 #endif
